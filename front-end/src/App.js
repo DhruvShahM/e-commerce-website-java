7a03c42a98d0702a19d27f75c39269eb73ea75e5
DhruvShah <dhruvshah78223@gmail.com>
2023-11-02 13:04:34 +0530

    react project intitate and integrate the static navigation.

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from './customer/coponentss/Navigations/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <div>
+        home
+      </div>
+    </div>
+  );
+}
+
+export default App;
